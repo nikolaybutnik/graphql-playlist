e53ac474c31672ab57eaadd0af823d2c2ba3ef8a
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-02-19 23:25:17 -0500

    ✨ Set up graphql connection with express

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,12 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 
 const app = express()
+
+// When a request to /graphql comes in, pass the operation to graphqlHTTP
+app.use('/graphql', graphqlHTTP({
+
+}))
 
 app.listen(4000, () => {
     console.log('Listening on port 4000.')
