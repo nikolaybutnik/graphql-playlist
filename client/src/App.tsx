6a60cfc1953c790c58ec9ad81f34185632c06cb9
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-02-22 22:42:14 -0500

    🗃 Set up Apollo Client for GraphQL interaction

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
+// Components
 import BookList from './components/BookList'
+
+// Apollo Client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      <div id="main">
-        <h1>Reading List</h1>
-        <BookList />
+    <ApolloProvider client={client}>
+      <div className="App">
+        <div id="main">
+          <h1>Reading List</h1>
+          <BookList />
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   )
 }
 
