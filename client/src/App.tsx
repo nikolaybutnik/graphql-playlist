88bcb005d490a276e42628a5dbb478a40ec80c41
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-02-22 21:47:58 -0500

    ✨ Implement new component to hold future book data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import BookList from './components/BookList'
+
 function App() {
   return (
     <div className="App">
       <div id="main">
         <h1>Reading List</h1>
+        <BookList />
       </div>
     </div>
   )
