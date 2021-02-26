2b7324bc6b3b43048ceb7dc612e726fbc178688d
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-02-25 21:50:02 -0500

    ✨ Create new BookDetails component

---
+++ client/src/components/BookDetails.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+// import { getBooksQuery } from '../queries/queries'
+
+const BookDetails = () => {
+  return (
+    <div id="book-details">
+      <p>Output book details here</p>
+    </div>
+  )
+}
+
+export default BookDetails
